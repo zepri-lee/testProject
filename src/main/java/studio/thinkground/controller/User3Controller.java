9dba3ff6ae6a6b39aa831f2cea5d3e1f9a12a839
zepri-lee <zepri123@gmail.com>
2024-04-19 09:11:25 +0900

    UriComponentsBuilder 사용

--- src/main/java/studio/thinkground/controller/User3Controller.java
+++ src/main/java/studio/thinkground/controller/User3Controller.java
@@ -1,5 +1,7 @@
 package studio.thinkground.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import studio.thinkground.dto.UserDTO3;
 import studio.thinkground.service.AbstractService;
@@ -17,10 +21,10 @@ public class User3Controller extends AbstractService {
 	
 //	private final Logger LOGGER = LoggerFactory.getLogger(User2Controller.class);	
 	
-    @Autowired
-    public void setRestTemplateBuilder(RestTemplateBuilder builder) {
-        setRestTemplate(builder.build());
-    }	
+//    @Autowired
+//    public void setRestTemplateBuilder(RestTemplateBuilder builder) {
+//        setRestTemplate(builder.build());
+//    }	
 
 	@PostMapping(value="user3")
 //	public UserDTO3 userJson(@RequestBody UserDTO3 userDto3) {
@@ -29,7 +33,17 @@ public class User3Controller extends AbstractService {
 		System.out.println("userDto3 : "+userDto3.toString());
 		
 //		return restTemplate.postForObject("http://localhost:8080/api/v1/userRest", userDto3, UserDTO3.class);
-		return restTemplate.postForEntity("http://localhost:8080/api/v1/userRest", userDto3, UserDTO3.class);
+//		return restTemplate.postForEntity("http://localhost:8080/api/v1/userRest", userDto3, UserDTO3.class);
+		
+		URI uri = UriComponentsBuilder
+				.fromUriString("http://localhost:8080")
+				.path("/api/v1/userRest")
+				.encode()
+				.build()
+				.toUri();
+		
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.postForEntity(uri, userDto3, UserDTO3.class);
 		
 	}
 	
